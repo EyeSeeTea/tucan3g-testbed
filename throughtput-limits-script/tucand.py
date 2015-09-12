e8d644c59bdb08cf11bb6ce4436038f6c7e3661a
ifoche <nacho.foche@gmail.com>
2015-09-12 16:26:32 +0200

    python daemon working without algorithms

--- throughtput-limits-script/tucand.py
+++ throughtput-limits-script/tucand.py
@@ -8,6 +8,7 @@ import time
 from os.path import join, isfile
 import json
 import ConfigParser
+import itertools
 
 #third party libs
 from daemon import runner
@@ -18,16 +19,16 @@ class TUCANDaemon():
         self.stdin_path = '/dev/null'
         self.stdout_path = '/dev/tty'
         self.stderr_path = '/dev/tty'
-        self.pidfile_path =  '/var/run/testdaemon/testdaemon.pid'
+        self.pidfile_path =  '/var/run/tucand.pid'
         self.pidfile_timeout = 5
            
     def run(self):
         # Extract some useful paths from config file
         try:
-            TUCANConfFolder = config.get('tucan', 'EtcFolder')
-            TUCANVarFolder = config.get('tucan', 'LogFolder')
-            TUCANTmpFolder = config.get('tucan', 'TmpFolder')
-            TUCANIpsFile = config.get('tests', 'confFile')
+            TUCANConfFolder = config.get('general', 'EtcFolder')
+            TUCANVarFolder = config.get('general', 'LogFolder')
+            TUCANTmpFolder = config.get('general', 'TmpFolder')
+            TUCANIpsFile = config.get('general', 'confFile')
         except:
             logger.setLevel(logging.ERROR)
             logger.error('Config file tucand.conf malformed. Mandatory fields missing!')
@@ -37,12 +38,12 @@ class TUCANDaemon():
         # we operate all the time using our memory cached file content.
         tests = dict()
         with open(TUCANIpsFile) as ipsConfFile:
-        # Save the content of the file in a dictionary where the key is the first entry of the file, and the rest of the line is stored as a list
-        for line in ipsConfFile:
-            # turn a list like ['a', 'b', 'c', 'd'] into a dict like {'a': ['b', 'c', 'd']}
-            key = line.split()[0]
-            values = line.split()[1:]
-            tests.update(dict(itertools.izip_longest(*[iter([key, values])] * 2, fillvalue="")))
+            # Save the content of the file in a dictionary where the key is the first entry of the file, and the rest of the line is stored as a list
+            for line in ipsConfFile:
+                # turn a list like ['a', 'b', 'c', 'd'] into a dict like {'a': ['b', 'c', 'd']}
+                key = line.split()[0]
+                values = line.split()[1:]
+                tests.update(dict(itertools.izip_longest(*[iter([key, values])] * 2, fillvalue="")))
                  
         while True:
             for key in tests.keys():
@@ -56,7 +57,7 @@ class TUCANDaemon():
                     receiverBw = data['end']['streams'][0]['receiver']['bits_per_second']
                     logger.info("key: %s -- IP_ORIG: %s -- IP_DST: %s -- DS: %s -- SENDER BW: %s, RECEIVER BW: %s" % (key, tests[key][0], tests[key][1], tests[key][2], senderBw, receiverBw))
                     # If it is an edge node we have to configure queues according to measured bw
-                    if config.getboolean('tucan', 'edge'):
+                    if config.getboolean('general', 'edge'):
                         logger.info('this is an edge node')
  
             #Note that logger level needs to be set to logging.DEBUG before this shows up in the logs
---
+++ throughtput-limits-script/tucand.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          tucand
+# Required-Start:    $local_fs $remote_fs $network $syslog $nocatsplash $ntp $TUCAN_ntp $TUCAN_owamp $TUCAN_bwctl $TUCAN_bwctl_tests
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# X-Interactive:     false
+# Short-Description: TUCAN3G daemon launcher
+# Description: TUCAN3G main daemon
+### END INIT INFO
+
+#
+# Copyright (c) 2015.
+#
+# This file is part of WP5 TUCAN3G Testbed
+#
+#  WP5 TUCAN3G Testbed software is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  WP5 TUCAN3G Testbed software is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  Script developed by EyeSeeTea Ltd
+#
+
+#######################
+# TUCAN3G main daemon #
+#######################
+
+now=$(date +"%F %k:%M:%S")
+log_tag="[TUCAN3G]"
+log_level="-p local0.notice"
+
+case "$1" in
+start)
+  # Check ips.conf and tucand.conf existance
+  
+  logger $log_level -t $log_tag -s "Launching daemon..."
+  python /etc/TUCAN3G/tucand.py start
+  logger $log_level -t $log_tag -s "Done"
+  ;;
+stop)
+  logger $log_level -t $log_tag -s "Stopping daemon..."
+  python /etc/TUCAN3G/tucand.py stop
+  logger $log_level -t $log_tag -s "Done"
+  ;;
+restart)
+  logger $log_level -t $log_tag -s "Restarting daemon..."
+  python /etc/TUCAN3G/tucand.py restart
+  logger $log_level -t $log_tag -s "Done"
+  ;;
+*)
+  log_level="-p local0.err"
+  logger $log_level -t $log_tag -s "[$now] - [FATAL] - Unknow command. Usage: /etc/init.d/tucand.sh [start|stop|restart]"
+  ;;
+esac
+
+exit 0
+
